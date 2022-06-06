da475d909ebe97d0d731547e232397d8867b0d5c
Claudine G. Bustillo <97108898+spaceqtt@users.noreply.github.com>
2022-06-06 18:55:52 +0800

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+USE [fdbform]
+GO
+
+INSERT INTO [dbo].[fdbform]
+           ([name]
+           ,[email]
+           ,[message])
+     VALUES
+           (<cname, varchar(50),>
+           ,<email, varchar(50),>
+           ,<cmessage, varchar(max),>)
+GO
+
+
